d9da790a13ec04ef700d5cf1c65a20b2855f72e1
Ubuntu ROS <yoan.tek@live.fr>
2017-09-30 00:37:00 -0700

    Ajout prise en main, camera et teleop

---
+++ prise_en_main/src/camera.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import rospy
+# ROS Image message
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+
+# Instantiate CvBridge
+bridge = CvBridge()
+
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+	cv2.imshow('camera',cv2_img)
+	cv2.waitKey(1)
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('image_listener', anonymous=True)
+
+    rospy.Subscriber("/bebop/image_raw", Image, image_callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
---
+++ prise_en_main/src/tele_op_drone.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import roslib;
+import rospy
+
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
+import sys, select, termios, tty
+
+msg= """Test 
+	test? 
+	"""
+mvtBindings = {
+		'z':(1.0,0,0,0),
+		's':(-1.0,0,0,0),
+		'q':(0,1.0,0,0),
+		'd':(0,-1.0,0,0),
+		'j':(0,0,0,1.0),
+		'l':(0,0,0,-1.0),
+		'i':(0,0,1.0,0),
+		'k':(0,0,-1.0,0),
+	       }
+
+
+def getKey():
+	tty.setraw(sys.stdin.fileno())
+	select.select([sys.stdin], [], [], 0)
+	key = sys.stdin.read(1)
+	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+	return key
+
+
+if __name__=="__main__":
+    	settings = termios.tcgetattr(sys.stdin)
+	
+	pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size = 1)
+	pubTakeoff = rospy.Publisher('/bebop/takeoff', Empty, queue_size = 1)
+	pubLand = rospy.Publisher('/bebop/land', Empty, queue_size = 1)
+	rospy.init_node('teleop_test', anonymous= True)
+	
+	start = raw_input("Take off? ")
+	if start == "yes":
+		
+		print("Take off")
+		pubTakeoff.publish()
+		
+		x = 0.0
+		y = 0.0
+		z = 0.0
+		th = 0.0
+		status = 0.0
+
+		try:
+			print msg
+			while(1):
+				key = getKey()
+				if key in mvtBindings.keys():
+					print(key)
+					x = mvtBindings[key][0]
+					y = mvtBindings[key][1]
+					z = mvtBindings[key][2]
+					th = mvtBindings[key][3]
+					if (status == 14):
+						print msg
+					status = (status + 1) % 15
+				else:
+					x = 0.0
+					y = 0.0
+					z = 0.0
+					th = 0.0
+					if (key == '\x03'):
+						break
+
+				twist = Twist()
+				twist.linear.x = x; twist.linear.y = y; twist.linear.z = z;
+				twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = th
+				print(twist)				
+				pub.publish(twist)
+
+		except:
+			print "Error! Exception!"
+
+		finally:
+			twist = Twist()
+			twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+			twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+			pub.publish(twist)
+			pubLand.publish()
+			print("Land!")	    		
+			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+	else:
+		print("No start")
+
