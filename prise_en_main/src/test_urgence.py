e0cd70d89446d372353dd8abeea5d110c47dc8a8
Ubuntu ROS <yoan.tek@live.fr>
2017-10-01 09:01:50 -0700

    Improved test_urgence

--- prise_en_main/src/test_urgence.py
+++ prise_en_main/src/test_urgence.py
@@ -4,11 +4,22 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
-import sys, select, termios, tty
+import sys, select, termios, tty, time
 
-msg= """Test 
-	test? 
+msg= """Mode Manuel 
+	Commande:
+		z : avancer
+		q : translation a gauche
+		s : reculer
+		d : translation a droite
+
+		i : monter
+		k : descendre
+
+		j : rotation trigonometrique
+		l : rotation horaire
 	"""
+
 mvtBindings = {
 		'z':(1.0,0,0,0),
 		's':(-1.0,0,0,0),
@@ -22,8 +33,13 @@ mvtBindings = {
 
 
 def autopilot():
+	#Only turn around the Z axis
+	twist = Twist()
+	twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+	twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 1.0
 	while (1):	
 		print "autopilot"
+		pub.publish(twist)
 
 def getKey():
 	tty.setraw(sys.stdin.fileno())
@@ -36,16 +52,16 @@ def getKey():
 if __name__=="__main__":
     	settings = termios.tcgetattr(sys.stdin)
 	
-	#pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size = 1)
-	#pubTakeoff = rospy.Publisher('/bebop/takeoff', Empty, queue_size = 1)
-	#pubLand = rospy.Publisher('/bebop/land', Empty, queue_size = 1)
-	#rospy.init_node('teleop_test', anonymous= True)
+	pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size = 1)
+	pubTakeoff = rospy.Publisher('/bebop/takeoff', Empty, queue_size = 1)
+	pubLand = rospy.Publisher('/bebop/land', Empty, queue_size = 1)
+	rospy.init_node('teleop_test_urgence', anonymous= True, disable_signals=True)
 	
 	start = raw_input("Take off? ")
 	if start == "yes":
 		
 		print("Take off")
-		#pubTakeoff.publish()
+		pubTakeoff.publish()
 		
 		x = 0.0
 		y = 0.0
@@ -54,6 +70,7 @@ if __name__=="__main__":
 		status = 0.0
 
 		try:
+			time.sleep(5)
 			autopilot()
 
 		except KeyboardInterrupt:
@@ -83,7 +100,7 @@ if __name__=="__main__":
 					twist.linear.x = x; twist.linear.y = y; twist.linear.z = z;
 					twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = th
 					print(twist)				
-					#pub.publish(twist)
+					pub.publish(twist)
 			except:
 				print ("Error! Exit")
 			
@@ -91,8 +108,8 @@ if __name__=="__main__":
 			twist = Twist()
 			twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
 			twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-			#pub.publish(twist)
-			#pubLand.publish()
+			pub.publish(twist)
+			pubLand.publish()
 			print("Land!")	    		
 			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 	else:
