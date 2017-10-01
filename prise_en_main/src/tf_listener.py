ce54f9fc8c6361a46afeb4e41a37835e32e4eba6
MeZigue <pierre.francolin@gmail.com>
2017-10-01 17:55:33 +0200

    Trying movment with tf listening

--- prise_en_main/src/tf_listener.py
+++ prise_en_main/src/tf_listener.py
@@ -4,18 +4,42 @@ import rospy
 import tf
 
 if __name__ == '__main__':
+
+	pub = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size = 1)
+	pubTakeoff = rospy.Publisher('/bebop/takeoff', Empty, queue_size = 1)
+	pubLand = rospy.Publisher('/bebop/land', Empty, queue_size = 1)
+
+	listener = tf.TransformListener()
+
 	rospy.init_node('bebop_tf_listener')
 	
-	listener = tf.TransformListener()
-	
-	rate = rospy.Rate(10.0)
-	while not rospy.is_shutdown():
+	print("Take off")
+	pubTakeoff.publish()
+
+	rate = rospy.Rate(3.0)
+
+	(trans,rot)=listener.lookupTransform('odom','point1',rospy.Time(0))
+
+	while trans[1]<2
 		try:
 			(trans,rot)=listener.lookupTransform('odom','base_link',rospy.Time(0))
+			twist = Twist()
+			twist.linear.y = 0.5
+			print("twist: ", twist)				
+			pub.publish(twist)
+			
+			
 
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 
+			
 			continue
 
-		print("trans: ",transf)
+		print("trans: ",trans)
 		print("rot: ", rot)
+
+	twist = Twist()
+	twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+	twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+	pub.publish(twist)
+	pubLand.publish()
