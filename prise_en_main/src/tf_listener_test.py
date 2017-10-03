48a65d2ac013a42754b7ce348644348ec0225b09
Pierre F <pierre.francolin@gmail.com>
2017-10-03 11:11:21 +0200

    Upgraded tf_listener_test to take any coordinate into account.\n NEED TO BE CHECKED AND TESTED ! \n A local copy of the old version of tf_listener_test was made locally on my computer.

--- prise_en_main/src/tf_listener_test.py
+++ prise_en_main/src/tf_listener_test.py
@@ -34,29 +34,105 @@ mvtBindings = {
 
 
 def autopilot():
+
+	#Rate to send data is 3Hz
 	rate = rospy.Rate(3.0)
 
 	(trans,rot)=listener.lookupTransform('odom','base_link',rospy.Time(0))
-
-	while trans[1]<2:
-		try:
-			(trans,rot)=listener.lookupTransform('odom','base_link',rospy.Time(0))
-			twist = Twist()
-			twist.linear.x = 0.0; twist.linear.z = 0.0
-			twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-			twist.linear.y = 0.5
-			print("twist: ", twist)				
-			pub.publish(twist)
+	
+	#Allowed error on position
+	epsilon=0.5
+	
+	confirm='n'
+	
+	while confirm == 'n' or confirm =='no'
+		print("Enter a [x;y;z] coordinate:\n")
+		
+		pointX = raw_input("X: ? (enter stay to 'stay' on current coordinate)\n")
+		pointY = raw_input("Y: ? (enter stay to 'stay' on current coordinate)\n")
+		pointZ = raw_input("Z: ? (enter stay to 'stay' on current coordinate)\n")
+		
+		#If nothing entered on X stay at current position
+		if not isinstance(pointX, float):
+			pointX = trans[0]
+			print("Will stay on current coordinate on X.\nIf you don't want to stay on position make sure you entered a float.")
+		
+		#If nothing entered on Y stay at current position
+		if not isinstance(pointY, float):
+			pointY = trans[1]
+			print("Will stay on current coordinate on Y.\nIf you don't want to stay on position make sure you entered a float.")
 			
+		#If nothing entered on Z stay at current position
+		if not isinstance(pointZ, float):
+			pointY = trans[2]
+			print("Will stay on current coordinate on Z.\nIf you don't want to stay on position make sure you entered a float.")
 			
+		confirm = raw_input("Do you want to go to [",pointX,",",pointY,",",pointY,"] (y/n)? (q to quit) \n")
+	
+	if confirm == "yes" || confirm == "y":
+		
+		#boolean for test position
+		onX = False
+		onY = False
+		onZ = False
+		
+		while (onX and onY and onZ)==False:
+			try:
+				
+				(trans,rot)=listener.lookupTransform('odom','base_link',rospy.Time(0))
+				
+				twist = Twist()
+				
+				#init twist X Y Z
+				twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+				twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+				
+				#Movement condition on X
+				if trans[0] < pointX-epsilon:
+				twist.linear.x = 0.5
+				
+				elif trans[0] > pointX+epsilon:
+				twist.linear.x = -0.5
+				
+				else
+				onX=True
+				
+				#Movement condition on Y
+				if trans[1] < pointY-epsilon:
+				twist.linear.y = 0.5
+				
+				elif trans[1] > pointY+epsilon:
+				twist.linear.y = -0.5
+				
+				else :
+				onY=True
+				
+				#Movement condition on Z
+				if trans[2] < pointZ-epsilon :
+				twist.linear.z = 0.5
+				
+				if trans[2] > pointZ+epsilon :
+				twist.linear.z = -0.5
+				
+				else :
+				onZ=False
+				
+				
+				print("twist: ", twist)				
+				pub.publish(twist)
+				
+				
 
-		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 
-			
-			continue
+				
+				continue
 
-		print("trans: ",trans)
-		print("rot: ", rot)
+			print("trans: ",trans)
+			print("rot: ", rot)
+		
+		(trans,rot)=listener.lookupTransform('odom','base_link',rospy.Time(0))
+		print("Arrived at target: [",trans[0],",",trans[1],",",trans[2],"] !")
 
 def getKey():
 	tty.setraw(sys.stdin.fileno())
