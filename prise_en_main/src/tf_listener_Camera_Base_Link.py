3f0af7b9173ccbf9b94e861824f33454a28cdab2
Pierre F <pierre.francolin@gmail.com>
2017-10-06 16:54:11 +0200

    Add tf listener camera base link

---
+++ prise_en_main/src/tf_listener_Camera_Base_Link.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import rospy
+# ROS tf
+import tf
+
+import time
+
+if __name__ == '__main__':
+
+	listener = tf.TransformListener()
+
+	rospy.init_node('bebop_tf_listener')
+
+	rate = rospy.Rate(3.0)
+
+	(trans,rot)=listener.lookupTransform('base_link','camera_base_link',rospy.Time(0))
+
+	while 1:
+		try:
+			(trans,rot)=listener.lookupTransform('base_link','camera_base_link',rospy.Time(0))
+            time.sleep(1)
+
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			
+			continue
+
+		print("trans: ",trans)
+		print("rot: ", rot)
