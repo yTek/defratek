91183119632554eee0190141a0e4d9469c85dbba
Pierre F <pierre.francolin@gmail.com>
2017-10-06 17:41:55 +0200

    Basic speed listener. Not tested

---
+++ prise_en_main/src/speed.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+# ROS Image message
+from nav_msgs.msg import Odometry
+
+#ODOM call back
+def odometry_callback(msg):
+    print(msg)
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('odometry_listener', anonymous=True)
+
+    rospy.Subscriber("/bebop/odom", Odometry, odometry_callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
