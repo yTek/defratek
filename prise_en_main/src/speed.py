d4f516e608b3e7412cbc8cdf55b8693ab1627d26
Pierre F <pierre.francolin@gmail.com>
2017-10-11 14:16:00 +0200

    Added pos extraction from speed info. Not tested

--- prise_en_main/src/speed.py
+++ prise_en_main/src/speed.py
@@ -7,17 +7,37 @@ import time
 from bebop_msgs.msg import Ardrone3PilotingStateSpeedChanged
 from bebop_msgs.msg import Ardrone3PilotingStateAltitudeChanged
 
-montemps=time.time()
+last_time=time.time()
+my_pos=(0.0,0.0,0.0)
 
 #ODOM call back
 def speed_callback(msg):
-	y=time.time()-montemps
-	print("Time: ", y)
-    print(msg)
+	
+	new_time=time.time()-last_time
+
+	delta_time=new_time-last_time
+
+    print("Speed: ",msg)
+
 	speedX=msg.speedX
+	deltaX=msg.speedX*delta_time
+
 	speedY=msg.speedY
+	deltaY=msg.speedY*delta_time
+
 	speedZ=msg.speedZ
-		
+	deltaZ=msg.speedZ*delta_time
+
+	update_pos(deltaX,deltaY,deltaZ)
+	
+def update_pos(deltaX,deltaY,deltaZ):
+	my_pos[0]=my_pos[0]+deltaX
+	my_pos[1]=my_pos[1]+deltaY
+	my_pos[2]=my_pos[2]+deltaZ
+	print("Speed: ")
+
+
+
 #ODOM call back
 def altitude_callback(msg):
 	print("Altitude: ")
@@ -33,6 +53,7 @@ def listener():
     rospy.init_node('speed_listener', anonymous=True)
 
     rospy.Subscriber("/bebop/states/ardrone3/PilotingState/SpeedChanged", Ardrone3PilotingStateSpeedChanged, speed_callback)
+	
 	rospy.Subscriber("/bebop/states/ardrone3/PilotingState/AltitudeChanged", Ardrone3PilotingStateAltitudeChanged, altitude_callback)
 	
 	
