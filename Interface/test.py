d6db678ca9b8d51cf377220aa465c412cf51b8da
Désilets Nicolas <goomboosh@hotmail.fr>
2017-10-25 17:57:57 +0200

    Add files via upload

--- Interface/test.py
+++ Interface/test.py
@@ -1,20 +1,28 @@
 from time import sleep
 import ttk
-from Tkinter import Tk, Label, Button, Frame, PhotoImage
+from Tkinter import *
 from PIL import ImageTk, Image
+import os
 
-class tabmanager():
+class term(Frame):
+	def __init__(self, master, size, color):
+		Frame.__init__(self, master, height=150, width=1500)
+		self.wid = self.winfo_id()
+		os.system('xterm -into %d -geometry %dx%d -fg %s -sb &' % (self.wid, size[0], size[1], color))
+
+
+class cameratabmanager():
 	def __init__(self,master,dronelist):
 		self.master=master
 		self.labellist={}
-		nb = ttk.Notebook(master)
+		self.nb = ttk.Notebook(master)
 		for drone in dronelist:
-			tab = ttk.Frame(nb)
+			tab = ttk.Frame(self.nb)
 
-			nb.add(tab, text=drone['name'])
-			self.labellist[drone['name']] = cameradisplay(tab, drone['image'], 0.5)
+			self.nb.add(tab, text=drone['name'])
+			self.labellist[drone['name']] = cameradisplay(tab, drone['image'], 0.7)
 
-		nb.grid(row=0,column=0,sticky='wn')
+		self.nb.grid(row=0,column=0,sticky='wn')
 
 class cameradisplay(Label):
 	def __init__(self,master,img,scale):
@@ -31,13 +39,19 @@ class mastergui:
 		self.master = master
 		master.title("A simple GUI")
 
-		self.tabs = tabmanager(master,dronelist)
+		self.tabs = cameratabmanager(master,dronelist)
 		
 		self.waypoints = Label(master, text="  WAYPOINTS MANAGER  ",bg='#0000FF')
 		self.waypoints.grid(row=0,column=1,sticky='wens')
 
-		self.terminal = Label(master, text="  TERMINAL  ",bg='#FF0000')
+		self.terminal = term(master,(246,15), "white")
 		self.terminal.grid(row=1,column=0,columnspan=2,sticky='wens')
+		
+		self.terminal2 = term(master,(246,15),"grey")
+		self.terminal2.grid(row=2,column=0,columnspan=2,sticky='wens')
+		
+		self.terminal3 = term(master,(246,15),"white")
+		self.terminal3.grid(row=3,column=0,columnspan=2,sticky='wens')
 
 if __name__ == "__main__":
 
@@ -71,5 +85,6 @@ if __name__ == "__main__":
 		#Camera hot reload
 		imagetemp={}
 		for drone in dronelist:
-			imagetemp[drone['name']] = ImageTk.PhotoImage(drone['image'].resize((int(drone['image'].size[0]*0.5),int(drone['image'].size[1]*0.5)), Image.ANTIALIAS))
-			gui.tabs.labellist[drone['name']].configure(image=imagetemp[drone['name']])
+			if drone['name'] == gui.tabs.nb.tab(gui.tabs.nb.select(), "text"):
+				imagetemp[drone['name']] = ImageTk.PhotoImage(drone['image'].resize((int(drone['image'].size[0]*0.7),int(drone['image'].size[1]*0.7)), Image.ANTIALIAS))
+				gui.tabs.labellist[drone['name']].configure(image=imagetemp[drone['name']])
