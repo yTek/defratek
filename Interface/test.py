a91bd23f77b42dad2b94ba739dc97c9aa2edc320
Désilets Nicolas <goomboosh@hotmail.fr>
2017-11-08 01:12:07 +0100

    Add files via upload

--- Interface/test.py
+++ Interface/test.py
@@ -4,6 +4,64 @@ from Tkinter import *
 from PIL import ImageTk, Image
 import os
 
+class simframe(Frame):
+	def __init__(self,master):
+		self.master=master
+
+		Frame.__init__(self, master)
+
+		self.startstopbutton=Button(self, text="Start Simulation")
+		self.takeoffbutton=Button(self, text="Takeoff")
+		self.landbutton=Button(self, text="Land")
+
+		self.startstopbutton.pack(side=LEFT, pady=4, fill=BOTH)
+		self.takeoffbutton.pack(side=LEFT, pady=4, fill=BOTH)
+		self.landbutton.pack(side=LEFT, pady=4, fill=BOTH)
+
+		
+class waypointinput(Frame):
+	def __init__(self,master):
+		self.master=master
+
+		Frame.__init__(self, master)
+
+		self.xinput=Entry(self, width=6)
+		self.yinput=Entry(self, width=6)
+		self.zinput=Entry(self, width=6)
+		self.minusbutton=Button(self, text="X")
+		self.upbutton=Button(self,text="^")
+		self.downbutton=Button(self,text="v")
+
+		self.xinput.pack(side=LEFT, padx=4)
+		self.yinput.pack(side=LEFT, padx=4)
+		self.zinput.pack(side=LEFT, padx=4)
+		self.minusbutton.pack(side=LEFT, padx=2)
+		self.upbutton.pack(side=LEFT)
+		self.downbutton.pack(side=LEFT)
+
+class waypointsmanager(Frame):
+	def __init__(self,master):
+		self.master=master
+		
+		Frame.__init__(self, master, borderwidth=2, relief=GROOVE)
+		
+		scrollbar = Scrollbar(self)
+		scrollbar.pack(side=RIGHT, fill=Y)
+
+		self.simframe=simframe(self)
+		self.simframe.pack()
+
+		self.waypointframe = Frame(self)
+		#self.waypointframe = Frame(self, yscrollcommand=scrollbar.set)
+		waypointlist = [waypointinput(self.waypointframe) for _ in range(14)]
+		for entry in waypointlist:
+			entry.pack()
+
+		self.waypointframe.pack(side=RIGHT, fill=BOTH)
+
+		#scrollbar.config(command=waypointframe.yview)
+		self.grid(row=0,column=2,sticky='wens')
+
 class term(Frame):
 	def __init__(self, master, size, color):
 		Frame.__init__(self, master, height=150, width=1500)
@@ -37,21 +95,23 @@ class cameradisplay(Label):
 class mastergui:
 	def __init__(self, master,dronelist):
 		self.master = master
-		master.title("A simple GUI")
-
+		master.title("THE BEST")
+		
+		
 		self.tabs = cameratabmanager(master,dronelist)
 		
-		self.waypoints = Label(master, text="  WAYPOINTS MANAGER  ",bg='#0000FF')
-		self.waypoints.grid(row=0,column=1,sticky='wens')
-
-		self.terminal = term(master,(246,15), "white")
+		#waypointsbim = Frame(master)
+		self.waypoint = waypointsmanager(master)
+		
+		
+		"""self.terminal = term(master,(246,15), "white")
 		self.terminal.grid(row=1,column=0,columnspan=2,sticky='wens')
 		
 		self.terminal2 = term(master,(246,15),"grey")
 		self.terminal2.grid(row=2,column=0,columnspan=2,sticky='wens')
 		
 		self.terminal3 = term(master,(246,15),"white")
-		self.terminal3.grid(row=3,column=0,columnspan=2,sticky='wens')
+		self.terminal3.grid(row=3,column=0,columnspan=2,sticky='wens')"""
 
 if __name__ == "__main__":
 
@@ -59,7 +119,7 @@ if __name__ == "__main__":
 	dronelist=[
 	{'name':'drone1','image': Image.open('mockimg.jpg')},
 	{'name':'drone2','image': Image.open('mockimg2.jpg')},
-	{'name':'drone3','image': Image.open('mockimg2.jpg')},
+	{'name':'drone3','image': Image.open('mockimg3.jpg')},
 	]
 
 	gui = mastergui(root,dronelist)
@@ -75,16 +135,19 @@ if __name__ == "__main__":
 		dronelist=[
 		{'name':'drone1','image': Image.open('mockimg.jpg')},
 		{'name':'drone2','image': Image.open('mockimg2.jpg')},
-		{'name':'drone3','image': Image.open('mockimg2.jpg')},
+		{'name':'drone3','image': Image.open('mockimg3.jpg')},
 		]
 
 		#Tabs hot reload
 		if not len(dronelist) is len(gui.tabs.labellist):
 			print('Faire un hotreload des tabs ici')
 
+		"""
 		#Camera hot reload
 		imagetemp={}
 		for drone in dronelist:
+			print(drone['name'] == gui.tabs.nb.tab(gui.tabs.nb.select(), "text"))
 			if drone['name'] == gui.tabs.nb.tab(gui.tabs.nb.select(), "text"):
 				imagetemp[drone['name']] = ImageTk.PhotoImage(drone['image'].resize((int(drone['image'].size[0]*0.7),int(drone['image'].size[1]*0.7)), Image.ANTIALIAS))
 				gui.tabs.labellist[drone['name']].configure(image=imagetemp[drone['name']])
+		"""
